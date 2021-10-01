696c2e4b5c141565659ada69a1d22043ab0225c6
viforget <victor.forget99@gmail.com>
2021-10-01 13:56:19 +0200

    Good thread when think

--- srcs/second_thread.c
+++ srcs/second_thread.c
@@ -5,11 +5,15 @@ void	*checker(void *tmp)
 	t_ph			*ph;
 
 	ph = (t_ph *)tmp;
+	while (!*(ph->tm_start))
+		;
+	*(ph->tm_lst_eat) = time_to_mili();
 	while(*(ph->tm_start))
 	{
 		if (*(ph->tm_lst_eat) + ph->info.tm_die < time_to_mili())
 		{
-			print_status(time_to_mili(), ph->index, DEAD, ph);
+			print_status(time_to_mili() - *ph->tm_start, ph->index, DEAD, ph);
+			*(ph->tm_start) = 0;
 		}
 	}
 	pthread_exit(NULL);
